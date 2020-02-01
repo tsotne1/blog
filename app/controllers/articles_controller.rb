3a4224444639eeda39a84032bfb552c427da9ec2
tsotne1 <tsotne.mazmishvili.1@btu.edu.ge>
2020-02-01 23:54:43 +0400

    add association betwen user and articles and debugger

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,7 +14,9 @@ class ArticlesController < ApplicationController
   end
 
   def create
+    debugger
     @article = Article.new(article_params)
+    #@article.user = User.first
     if @article.save
       flash[:success] = "Article was created succ"
       redirect_to article_path(@article)
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ActiveRecord::Base
+  belongs_to :user
   validates :title, presence: true, length: {minimum: 3, maxmimum: 50}
   validates :description, presence: true, length: {minimum: 3, maxmimum: 50}
+  validates :user_id, presence: true
 end
