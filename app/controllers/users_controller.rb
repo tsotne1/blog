9fca31beaa75087a5ba3fe95824a67eb0fdd390a
tsotne1 <tsotne.mazmishvili.1@btu.edu.ge>
2020-02-04 00:28:15 +0400

    signiin after signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,8 +12,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to the blog #{@user.username}"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render 'new'
     end
@@ -34,8 +35,6 @@ class UsersController < ApplicationController
   def show
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
-
-
 
   private
   def user_params
