dace4deac513a1956dfde5a08266b46a1d1468e7
tsotne1 <tsotne.mazmishvili.1@btu.edu.ge>
2020-02-05 21:25:58 +0400

    update navbar with categories

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,7 +6,7 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
   def sign_in_as(user, password)
-    post_login_path, params:{session:{email: user.email, password: password}}
+    post login_path, params:{session:{email: user.email, password: password}}
   end
   # Add more helper methods to be used by all tests here...
 end
